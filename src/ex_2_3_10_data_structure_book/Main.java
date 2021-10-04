d25f50d95e949912a4402012a5dbabbb58b36246
Fael123Programming <rafaelfonseca1020@gmail.com>
2021-10-04 16:21:52 -0300

    New Changes

--- src/ex_2_3_10_data_structure_book/Main.java
+++ src/ex_2_3_10_data_structure_book/Main.java
@@ -11,10 +11,10 @@ public class Main {
     public static void main(String[] args) {
         Register rg = new Register();
         System.out.println(rg.process("A B C * + D E - /") );
-        System.out.println("Then, the result is " + rg.getResult() + "\n");
+        System.out.println("Then, the result is in " + rg.getResult() + "\n");
         System.out.println(rg.process("Z Y * I O + P W / - +") );
-        System.out.println("Then, the result is " + rg.getResult() + "\n");
+        System.out.println("Then, the result is in " + rg.getResult() + "\n");
         System.out.println(rg.process("Q W B + *"));
-        System.out.println("Then, the result is " + rg.getResult());
+        System.out.println("Then, the result is in " + rg.getResult());
     }
 }
--- src/ex_2_3_10_data_structure_book/Register.java
+++ src/ex_2_3_10_data_structure_book/Register.java
@@ -12,48 +12,49 @@ import java.util.Stack;
  * -> DV A: to divide register content by A and to assign register with the result of it;
  */
 public class Register {
-    private Stack <String> operands;
+    private final Stack<String> operands;
 
-    public Register(){ this.operands = new Stack<>(); }
+    public Register() {
+        this.operands = new Stack<>();
+    }
 
-    public String getResult(){
-        if( this.operands.isEmpty() ) return null;
+    public String getResult() {
+        if (this.operands.isEmpty()) return null;
         return this.operands.peek();
     }
 
-    public String process( String postfixExp ) {
+    public String process(String postfixExp) {
         this.operands.clear();
-        String result = "", op1, op2;
+        StringBuilder result = new StringBuilder();
+        String op1, op2;
         int counterOfTempVars = 1;
-        for ( String perExp : postfixExp.split("") ){ //Z Y * I O + P W / - +
-            if ( perExp.equals(" ")) continue;
-            if ( this.isOperand( perExp ) ) this.operands.push(perExp);
-            else if( this.isOperator( perExp )) {
+        for (char perExp : postfixExp.toCharArray()) {
+            if (perExp == ' ') continue;
+            if (this.isOperand(perExp)) this.operands.push(Character.toString(perExp));
+            else if (this.isOperator(perExp)) {
                 op2 = this.operands.pop();
                 op1 = this.operands.pop();
-                result += "LD " + op1 + "\n";
-                switch( perExp ) {
-                    case "+":
-                        result += "AD " + op2 + "\n";
-                        break;
-                    case "-":
-                        result += "SB " + op2 + "\n";
-                        break;
-                    case "*":
-                        result += "ML " + op2 + "\n";
-                        break;
-                    case "/":
-                        result += "DV " + op2 + "\n";
-                        break;
+                result.append("LD ").append(op1).append("\n");
+                switch (perExp) {
+                    case '+' -> result.append("AD ");
+                    case '-' -> result.append("SB ");
+                    case '*' -> result.append("ML ");
+                    case '/' -> result.append("DV ");
                 }
-                result += "ST TEMP" + counterOfTempVars + "\n";
+                result.append(op2).append("\n");
+                result.append("ST TEMP").append(counterOfTempVars).append("\n");
                 this.operands.push("TEMP" + counterOfTempVars++);
             }
         }
-        return result;
+        return result.toString();
     }
 
-    private boolean isOperator(String operator){ return "*/+-".contains(operator);}
+    private boolean isOperator(char operator) {
+        return operator == '*' || operator == '/' || operator == '+' || operator == '-';
+    }
 
-    private boolean isOperand( String operand ) { return "abcdefghijklmnopqrstuvxwyz".contains(operand.toLowerCase());}
+    private boolean isOperand(char operand) {
+        operand = Character.toLowerCase(operand);
+        return operand >= 'a' && operand <= 'z';
+    }
 }
